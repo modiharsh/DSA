454269b09a225b215701ff61741cc4ad9a711c45
Harsh Modi <modiharsh21@gmail.com>
2025-07-28 11:25:00 +0530

    Create LC_64_Min_Path_Sum.py

---
+++ DynamicProgramming/LC_64_Min_Path_Sum.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/minimum-path-sum #Medium #Array #DynamicProgramming #Matrix
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # Method 1 : Using 2D space (m.n)
+        m = len(grid) 
+        n = len(grid[0]) 
+
+        dp = [[float(inf)] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = grid[i][0] + dp[i-1][0]
+
+        for j in range(1, n):
+            dp[0][j] = grid[0][j] + dp[0][j-1]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+  
+        return dp[m-1][n-1]
+
+        # Method 2: Using 1 D space(n)
+        # m, n = len(grid), len(grid[0])
+        # dp = [0] * n
+
+        # dp[0] = grid[0][0]
+        # for j in range(1, n):
+        #     dp[j] = dp[j - 1] + grid[0][j]
+
+        # for i in range(1, m):
+        #     dp[0] += grid[i][0]
+        #     for j in range(1, n):
+        #         dp[j] = min(dp[j], dp[j - 1]) + grid[i][j]
+
+        # return dp[-1]
+
+
+# Example 1:
+# Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+# Output: 7
+# Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+# Example 2:
+# Input: grid = [[1,2,3],[4,5,6]]
+# Output: 12
