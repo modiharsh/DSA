115e9eff8f823d817ed7fd01ee87d832f5dd4bf0
Harsh Modi <modiharsh21@gmail.com>
2025-08-15 09:34:48 +0530

    Create LC_342.py

---
+++ LC_342.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/power-of-four #Easy #Math #BitManipulation #Recurssion
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 4 == 0:
+            n //= 4
+        return n == 1
+
+# Example 1:
+# Input: n = 16
+# Output: true
+# Example 2:
+# Input: n = 5
+# Output: false
+# Example 3:
+# Input: n = 1
+# Output: true
